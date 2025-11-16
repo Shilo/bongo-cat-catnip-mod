9b580fa9786b8df97f850b59bc3dc7045a6af0a4
Shilo <shilo86@gmail.com>
2025-11-16 11:02:36 -0800

    feat: adds core mod functionality
    Loads mod as a Unity GameObject and MonoBehavior.

--- CatnipMod/CatnipMod.cs
+++ CatnipMod/CatnipMod.cs
@@ -1,6 +1,17 @@
-﻿namespace CatnipMod
+﻿using UnityEngine;
+
+namespace CatnipMod
 {
-    public class CatnipMod
-    {
-    }
+	public class CatnipMod : MonoBehaviour
+	{
+		private void Awake()
+		{
+			Debug.Log("[CatnipMod] Mod initialized.");
+		}
+
+		private void OnDestroy()
+		{
+			Debug.Log("[CatnipMod] Mod unloaded.");
+		}
+	}
 }
--- CatnipMod/Entrypoint.cs
+++ CatnipMod/Entrypoint.cs
@@ -1,6 +1,8 @@
 ﻿using HarmonyLib;
+using IroxGames.StoreFronts.Steam;
 using Steamworks;
 using System;
+using UnityEngine;
 
 namespace Doorstop
 {
@@ -14,12 +16,32 @@ namespace Doorstop
 			harmony.PatchAll();
 		}
 
-		// Workaround fix for Doorstop bug:
-		//   If Steam is not running on direct game launch, Doorstop environment variables never get unset.
-		//   After game executable restarts via Steam, the environment variables are still set,
-		//   causing Doorstop to skip initialization and Catnip mod never loads.
-		//   Without this fix, users would have to restart Steam to load Catnip mod again.
-		//   https://github.com/NeighTools/UnityDoorstop/issues/34
+		[HarmonyPatch(typeof(SteamManager), "Awake")]
+		private static class SteamManagerAwakePatch
+		{
+			private static bool _catnipModCreated = false;
+
+			static void Postfix()
+			{
+				if (_catnipModCreated)
+					return;
+
+				var catnipMod = new GameObject(nameof(CatnipMod.CatnipMod));
+				catnipMod.AddComponent<CatnipMod.CatnipMod>();
+				UnityEngine.Object.DontDestroyOnLoad(catnipMod);
+
+				_catnipModCreated = true;
+			}
+		}
+
+		/// <summary>
+		///	Workaround fix for Doorstop bug:
+		///   If Steam is not running on direct game launch, Doorstop environment variables never get unset.
+		///   After game executable restarts via Steam, the environment variables are still set,
+		///   causing Doorstop to skip initialization and Catnip mod never loads.
+		///   Without this fix, users would have to restart Steam to load Catnip mod again.
+		///   https://github.com/NeighTools/UnityDoorstop/issues/34
+		/// </summary>
 		private static void UnsetDoorstopEnvVarsIfNotSteamRunning()
 		{
 			if (SteamAPI.IsSteamRunning())
