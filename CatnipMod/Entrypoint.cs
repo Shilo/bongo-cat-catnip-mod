4e32e9f88f3f53a48b715bfbc2eb0e71759f06fd
Shilo <shilo86@gmail.com>
2025-11-16 09:55:39 -0800

    fix: prevents Doorstop init failure on direct game launch
    Unsets Doorstop environment variables if Steam is not running to prevent initialization failure.

--- CatnipMod/Entrypoint.cs
+++ CatnipMod/Entrypoint.cs
@@ -1,13 +1,33 @@
 ﻿//using HarmonyLib;
 
+using Steamworks;
+using System;
+
 namespace Doorstop
 {
 	internal class Entrypoint
 	{
 		public static void Start()
 		{
+			UnsetDoorstopEnvVarsIfNotSteamRunning();
+
 			//var harmony = new Harmony("com.shilo.catnipmod");
 			//harmony.PatchAll();
+		}
+
+		// Workaround fix for Doorstop bug:
+		//   If Steam is not running on direct game launch, Doorstop environment variables never get unset.
+		//   After game executable restarts via Steam, the environment variables are still set,
+		//   causing Doorstop to skip initialization and Catnip mod never loads.
+		//   Without this fix, users would have to restart Steam to load Catnip mod again.
+		//   https://github.com/NeighTools/UnityDoorstop/issues/34
+		private static void UnsetDoorstopEnvVarsIfNotSteamRunning()
+		{
+			if (SteamAPI.IsSteamRunning())
+				return;
+
+			Environment.SetEnvironmentVariable("DOORSTOP_DISABLE", null);
+			Environment.SetEnvironmentVariable("DOORSTOP_INITIALIZED", null);
 		}
 	}
 }
